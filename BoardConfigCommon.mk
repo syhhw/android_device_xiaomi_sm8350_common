2985ea7e49c2914b1ef708cd3d30e1dd0372db50
ChrisW444 <wandersonrodriguesf1@gmail.com>
2022-02-11 22:51:17 +0000

    sm8350-common: Build permissive for now
    Signed-off-by: ChrisW444 <wandersonrodriguesf1@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,9 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
